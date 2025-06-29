21e9ae8e2a1d5f666460a8e4b3664205ba603949
ElPresedente <vova171002@gmail.com>
2025-06-29 13:34:54 +0300

    tmp, небольшие наброски по рулетке

--- logic/roulette.go
+++ logic/roulette.go
@@ -5,6 +5,8 @@ import (
 	"time"
 )
 
+type DonateEvent = sources.DonationEvent
+
 type RouletteSectorItem struct { //может потом еще что то понадобится (отдельные шансы для элемента внутри????)
 	name string
 }
@@ -24,5 +26,44 @@ type Roulette struct {
 	roll_price int
 	timeout    time.Time
 
-	queue []sources.DonationEvent
+	queue      []DonateEvent
+	stop       chan struct{}
+}
+
+func (a *Roulette) rouletteLoop(){
+	lastRoll := time.Now()
+	for{
+		if a.queue.empty(){
+			return
+		}
+		select{
+			case <- a.stop:
+				return
+			default:
+				if time.Since( lastRoll ) >= timeout{
+					//прокрутить рандомно число от 1 до 100 
+					//динамически присвоить каждому сектору промежуток чисел
+					//получить сектор, получить варианты сектора
+					//выбрать случайно один из вариантов
+					//выдать событие рулетки
+
+					//и всё это я не могу сделать без инета потому что не знаю как пользоваться этим языком
+					
+				} else{
+					time.Sleep( timeout /* - time.Since( lastRoll )*/)
+				} 
+		}
+	}
+}
+
+func (a *Roulette) process(){
+	if !queue.empty(){
+		return
+	}
+	go rouletteLoop();
+}
+
+func (a *Roulette) EnqueueDonate( event *DonateEvent ){
+	//queue.emplace_back( event ) не знаю как правильно)
+	process();	
 }
